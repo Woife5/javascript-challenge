324bc93d1085ab4630f1243b771839e571407510
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2021-11-29 23:30:41 +0100

    Added solution 13

--- hackernoon/challenge_13.js
+++ hackernoon/challenge_13.js
@@ -1,3 +1,7 @@
 // Find the maximum number in an array of numbers
 
-const numbers = [2, 3, 8, 4, 6, 1];
+const numbers = [2, 3, 8, 4, 6, 10];
+
+const max = numbers.reduce((prev, cur) => Math.max(prev, cur));
+
+console.log(`Max number: ${max}`);
